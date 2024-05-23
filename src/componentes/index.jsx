f84b4aea47d48bba5add0b58aa3d231fce82e930
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-05-23 19:06:08 -0300

    adicionado mais informações

--- src/componentes/index.jsx
+++ src/componentes/index.jsx
@@ -5,11 +5,19 @@ const Valor = () => {
         <div className="container">
             <h1 className="titulo">VALOR TOTAL ATÉ O MOMENTO: </h1>
             <h2 className="valor">
-                R$ 126,91
+                R$ 40,21
             </h2>
             <img src='/imagens/qr.jpg' alt='Logo do Organo'/>
             <p className="p"><strong>Chave Pix </strong>39514655-1493-44cf-ba9c-2f0b78777ccf</p>
-
+            <h2>O que precisamos fazer:</h2>
+            <ul>
+                <li>Rifar a lâmpada</li>
+                <li>Sortear a lâmpada. Sorteio: 13/06/2024</li>
+            </ul>
+            <h2>O que foi feito:</h2>
+            <ul>
+                <li>Lâmpada inteligente comprada</li>
+            </ul>
         </div>
     )
 }
